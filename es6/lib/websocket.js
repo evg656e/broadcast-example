ad910eaefa54e9a8d1b4718d1bf9eba97870c375
evg656e <evg656e@gmail.com>
2017-08-18 16:14:44 +0300

    corrections

--- es6/lib/websocket.js
+++ es6/lib/websocket.js
@@ -1,15 +1,4 @@
-export default (function() {
-    if (typeof Qt === 'object') {
-        return require('polyfill-qml/lib/websocket');
-    }
-
-    if (typeof WebSocket !== 'undefined') {
-        return WebSocket;
-    }
-
-    if (typeof exports === 'object' && typeof module === 'object') {
-        //! \see https://stackoverflow.com/a/41063795/2895579
-        //! \see https://www.npmjs.com/package/ws
-        return require('ws');
-    }
-}());
+export default
+    typeof WebSocket === 'function' ? WebSocket :
+    typeof Qt === 'object'          ? require('polyfill-qml/lib/websocket') :
+                                      require('ws');
--- ts/lib/websocket.ts
+++ ts/lib/websocket.ts
@@ -1,14 +1,7 @@
 declare const Qt: any;
 declare function require(id: string): any;
 
-export default (function() {
-    if (typeof Qt === 'object') {
-        return require('polyfill-qml/lib/websocket');
-    }
-
-    if (typeof WebSocket !== 'undefined') {
-        return WebSocket;
-    }
-
-    return require('ws');
-}());
+export default
+    typeof WebSocket === 'function' ? WebSocket :
+    typeof Qt === 'object'          ? require('polyfill-qml/lib/websocket') :
+                                      require('ws');
